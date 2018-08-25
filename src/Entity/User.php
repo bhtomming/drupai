5ca0e9dca588d49cf5136a50db5df29d8a2ed6f7
tomming <233238526@qq.com>
2018-08-25 11:43:42 +0800

    添加用户登录及管理后台

---
+++ src/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by Drupai.
+ * User: 烽行天下
+ * Date: 2018\8\8 0008
+ * Time: 16:50
+ */
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="drupai_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
