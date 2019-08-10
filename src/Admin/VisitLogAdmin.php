1c90c3c8b7ba277a0d4de95df3ee330c9c9bd427
bhtomming <233238526@qq.com>
2019-08-10 14:29:13 +0800

    修改主页自适应

--- src/Admin/VisitLogAdmin.php
+++ src/Admin/VisitLogAdmin.php
@@ -25,6 +25,7 @@ class VisitLogAdmin extends AbstractAdmin
         $list->add('username',null,['label'=>'用户'])
             ->add('currentUrl',null,['label'=>'访问'])
             ->add('referrer',null,['label'=>'来源'])
+            ->add('ip',null,['label'=>'来访IP'])
             ->add('action',null,['label'=>'方式'])
             ->add('createdAt',null,[
                 'label'=>'时间',
